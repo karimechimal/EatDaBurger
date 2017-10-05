077a48f11e5932df3c74ad8f762bcd8023cf3a05
Karime Chimal <KarimeChimal@Karime-Chimals-MacBook-Pro-2.local>
2017-10-05 18:06:56 -0500

    thomas is here

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var methodOverride = require("method-override");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+app.use(bodyParser.text({ type: 'text/html' }));
+
+app.use(methodOverride('_method'));
+
+var exphbs = require('express-handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
+
+app.listen(PORT, function() {
+    console.log("app listening on PORT " + PORT);
+});
